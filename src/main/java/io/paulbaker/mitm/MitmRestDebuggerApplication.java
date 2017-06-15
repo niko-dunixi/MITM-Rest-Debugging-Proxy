81f0a8d6bdd085c1ed3b4fd5b702eb1d083d8752
Paul Baker <paul.nelson.baker@gmail.com>
2017-06-15 14:54:50 -0600

    Adding locating of cacerts file to code

--- src/main/java/io/paulbaker/mitm/MitmRestDebuggerApplication.java
+++ src/main/java/io/paulbaker/mitm/MitmRestDebuggerApplication.java
@@ -22,7 +22,17 @@ public class MitmRestDebuggerApplication implements CommandLineRunner {
     @Override
     public void run(String... strings) throws Exception {
         log.info("Good to go!");
-        File pemFile = new File(proxyProperties.getKeystoreDirectory(), proxyProperties.getAlias() + ".pem");
+        String alias = proxyProperties.getAlias();
+        File pemFile = new File(proxyProperties.getKeystoreDirectory(), alias + ".pem");
         log.info("Make sure that you import the " + pemFile + " file into your keystore!");
+        File cacertsFile = new File(System.getProperty("java.home"), "lib/security/cacerts");
+        log.info("Default cacerts file exists? " + cacertsFile.exists());
+        if (cacertsFile.exists()) {
+            log.info("Import into cacerts with this command:");
+            log.info("keytool -import -alias \"" + alias + "\" -keystore \"" + cacertsFile + "\" -file \"" + pemFile + "\"");
+        } else {
+            log.info("I cannot find your default cacerts file. You'll need to located it manually, and then run:");
+            log.info("keytool -import -alias \"" + alias + "\" -keystore \"path to your cacerts file\" -file \"" + pemFile + "\"");
+        }
     }
 }
